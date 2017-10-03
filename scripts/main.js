2f0e9e94ec1ccd4d25b758e00c5d175441c7224a
Peter Casey <pcasey@cocc.edu>
2017-10-03 12:39:22 -0700

    Added buttons and text fields

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,24 @@
+/*
+    Peter Casey
+    v 0.0001
+    10/3/17
+*/
+
 // Simple function to test our setup
 function test() {
     alert("hello world");
+}
+
+function readText() {
+    // Grab the input element from the document
+    let inputItem = document.getElementById("stuff");
+    // Grab the text from the input element
+    let theText = inputItem.value;
+    // Demonstrate...
+    alert("value is " + theText);
+    let theTextUpper = theText.toUpperCase();
+ //   inputItem.value = theTextUpper;
+    // Grab the output element from the document
+    let outputItem = document.getElementById("stuff2");
+    outputItem.value = theTextUpper;        
 }
